0fd92547f590eddc0048385ff31adb25950ff486
msaidztrk <81260057+msaidztrk@users.noreply.github.com>
2025-12-24 15:33:04 +0300

    feat(users): add UsersService with password hashing and conflict check

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,35 @@
+
+import { Injectable, ConflictException } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository) { }
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const existingUser = await this.usersRepository.findByEmail(createUserDto.email);
+        if (existingUser) {
+            throw new ConflictException('User with this email already exists');
+        }
+
+        if (createUserDto.password) {
+            const salt = await bcrypt.genSalt();
+            createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+        }
+
+        const { password, ...userData } = createUserDto;
+        const userToCreate = this.usersRepository.create({
+            ...userData,
+            passwordHash: password,
+        });
+
+        return this.usersRepository.save(userToCreate);
+    }
+
+    async findOneByEmail(email: string): Promise<User | null> {
+        return this.usersRepository.findByEmail(email);
+    }
+}
