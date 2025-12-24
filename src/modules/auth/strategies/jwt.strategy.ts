6fca4666b87ac763e5fe8868d32c9e51a16fb4f9
msaidztrk <81260057+msaidztrk@users.noreply.github.com>
2025-12-24 16:41:19 +0300

    feat(auth): implement jwt strategy and custom auth guard

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private configService: ConfigService,
+        private usersService: UsersService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET') || 'secret',
+        });
+    }
+
+    async validate(payload: any) {
+        // Determine what to return in request.user
+        // Usually payload.sub is userId
+        return { userId: payload.sub, email: payload.email };
+    }
+}
